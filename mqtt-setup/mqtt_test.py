1d2250583fc3328c1312510e5b7957184d3dd912
ChongYiCheng <chong.yi.cheng@gmail.com>
2020-06-14 15:14:01 +0800

    Set up MQTT

---
+++ mqtt-setup/mqtt_test.py
@@ -0,0 +1,16 @@
+#Following tutorial to send and receive JSON over MQTT with Python
+
+import json
+
+# #brokers
+# brokers_out = {"broker1":"192.168.1.206",
+#                 "broker2":"test.mosquitto.org",
+#                 "broker3":"iot.eclipse.org"}
+
+# #encode python dictionary or list
+# data_out = json.dumps(brokers_out)
+# data_in = data_out
+
+# brokers_in = json_loads(data_in)
+# #Convert from JSON to python 
+# m_in = json.loads(m_decode)
